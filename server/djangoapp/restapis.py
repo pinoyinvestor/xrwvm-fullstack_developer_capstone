03ac4c7aac4847ceb9d362ecb17d225c173f08ca
pinoyinvestor <dhbergs@proton.me>
2024-08-01 03:31:07 -0400

    Lagt till nya ändringar och migreringar

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -1,13 +1,18 @@
 import os
 import requests
 from dotenv import load_dotenv
+import logging
+
+# Konfigurera loggning
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 # Ladda miljövariabler från .env-fil
 load_dotenv()
 
 # Backend URL och Sentiment Analyzer URL från miljövariabler
-backend_url = os.getenv('backend_url', default="https://danielhedenb-3030.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai")
-sentiment_analyzer_url = os.getenv('sentiment_analyzer_url', default="http://localhost:5050/")
+backend_url = os.getenv('BACKEND_URL', "https://danielhedenb-3030.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai")
+sentiment_analyzer_url = os.getenv('SENTIMENT_ANALYZER_URL', "https://sentianalyzer.1k1wccn9oz0e.us-south.codeengine.appdomain.cloud/")
 
 def get_request(endpoint, **kwargs):
     # Bygg query-string från kwargs
@@ -15,7 +20,7 @@ def get_request(endpoint, **kwargs):
     
     # Bygg URL med query-string
     request_url = f"{backend_url}{endpoint}?{params}"
-    print(f"GET from {request_url}")
+    logger.info(f"GET from {request_url}")
     
     try:
         # Gör GET-anrop till URL:en
@@ -25,13 +30,13 @@ def get_request(endpoint, **kwargs):
         return response.json()
     except requests.RequestException as e:
         # Hantera alla nätverks- eller HTTP-fel
-        print(f"Network exception occurred: {e}")
+        logger.error(f"Network exception occurred: {e}")
         return None
 
 def analyze_review_sentiments(text):
     # URL för sentimentanalys
     request_url = f"{sentiment_analyzer_url}analyze/{text}"
-    print(f"GET from {request_url}")
+    logger.info(f"GET from {request_url}")
 
     try:
         # Gör GET-anrop till sentimentanalys URL:en
@@ -41,13 +46,13 @@ def analyze_review_sentiments(text):
         return response.json()
     except requests.RequestException as e:
         # Hantera alla nätverks- eller HTTP-fel
-        print(f"Network exception occurred: {e}")
+        logger.error(f"Network exception occurred: {e}")
         return None
 
 def post_review(data_dict):
     # URL för att posta en recension
     request_url = f"{backend_url}reviews"
-    print(f"POST to {request_url} with data {data_dict}")
+    logger.info(f"POST to {request_url} with data {data_dict}")
 
     try:
         # Gör POST-anrop med data
@@ -57,5 +62,5 @@ def post_review(data_dict):
         return response.json()
     except requests.RequestException as e:
         # Hantera alla nätverks- eller HTTP-fel
-        print(f"Network exception occurred: {e}")
+        logger.error(f"Network exception occurred: {e}")
         return None
