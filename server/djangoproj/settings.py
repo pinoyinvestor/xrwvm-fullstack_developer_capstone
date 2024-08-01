c72342cc63d4e81d1fff136609d445132aaf95f5
pinoyinvestor <dhbergs@proton.me>
2024-08-01 08:27:32 -0400

    Fix trailing whitespace issue in settings.py

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -1,4 +1,3 @@
-# Adjust lines that are too long and remove trailing whitespace
 """
 Django settings for djangoproj project.
 
@@ -14,10 +13,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from pathlib import Path
 
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -31,11 +28,12 @@ DEBUG = True
 ALLOWED_HOSTS = [
     'localhost',
     'danielhedenb-8000.theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai',
-    'danielhedenb-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
+    'danielhedenb-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai',
 ]
+
 CSRF_TRUSTED_ORIGINS = [
-    'https://danielhedenb-8000.theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai', 
-    'https://danielhedenb-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
+    'https://danielhedenb-8000.theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai',
+    'https://danielhedenb-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai',
 ]
 
 REST_FRAMEWORK = {
@@ -88,7 +86,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'djangoproj.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
@@ -114,7 +111,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
@@ -127,7 +123,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
